b2f85dc3d2cbf024a63e24caaf03bddf6e5c048f
hyosikchoi <huchoi24@naver.com>
2023-02-19 22:24:04 +0900

    Add - Sample design button

--- app/src/main/java/com/hyosik/android/designsystemex/MainActivity.kt
+++ app/src/main/java/com/hyosik/android/designsystemex/MainActivity.kt
@@ -2,10 +2,19 @@ package com.hyosik.android.designsystemex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.hyosik.android.designsystemex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.btnPrimarySmall.setOnClickListener {  }
+        binding.btnSecondarySmall.setOnClickListener {  }
+
     }
 }
